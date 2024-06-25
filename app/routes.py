a4b12e3f7f915fd9c129d95d5a0f125ae60609b7
Daisha <daisha05311994@gmail.com>
2024-06-24 20:16:04 -0400

    FSDIcommit

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def about():
+    me={
+        "first_name": "Daisha",
+        "last_name": "McCutcheon",
+        "hobbies": "All things art",
+        "is_online": True
+    }
+    return me
---
+++ run_local.sh
@@ -0,0 +1,7 @@
+
+
+export FLASK_APP=app/routes.py
+export FLASK_ENV=developement
+export DEBUG=1
+
+flask run
